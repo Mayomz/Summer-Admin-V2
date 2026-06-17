ad5fdcf3cca191c9fdcc72375f97c36283c81924
Mayomz <nvthegod@gmail.com>
2026-06-17 07:13:44 +0700

    Configure Supabase connection

--- assets/site-config.js
+++ assets/site-config.js
@@ -1,7 +1,7 @@
 window.FIVEM_TICKET_CONFIG = {
-  databaseProvider: "local",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  databaseProvider: "supabase",
+  supabaseUrl: "https://qaryecyiotcpqpbqhuaf.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhcnllY3lpb3RjcHFwYnFodWFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2Mzk2MTUsImV4cCI6MjA5NzIxNTYxNX0._Qv-XumlvRHFWB6B1F6fDk2dygg28mgYCfJy1GS1ZJY",
   supabaseStateTable: "app_state",
   supabaseStateKey: "main"
 };
